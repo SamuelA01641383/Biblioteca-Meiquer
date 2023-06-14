4fb24c3137f15a62498fdc166e63d0c3744215b8
Samuel117 <samuel.padilla45@gmail.com>
2023-06-13 19:31:30 -0600

    videos

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
 
 // Alumnos solo pueden acceder a videos y libros
 app.get("/Alumno", (req, res)=>{
-    const q = "SELECT nombre FROM contenido WHERE tipo=1 OR tipo=2;"
+    const q = "SELECT * FROM contenido WHERE tipo=1 OR tipo=2;"
     db.query(q,(err,data)=>{
         if(err) return res.json(err)
         return res.json(data)
