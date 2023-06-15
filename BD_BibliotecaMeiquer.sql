f29831c5301aa78ec7027884ef162d00e9b8a68f
Dhali21 <135290336+Dhali21@users.noreply.github.com>
2023-06-14 20:58:47 -0600

    Update BD_BibliotecaMeiquer.sql

--- BD_BibliotecaMeiquer.sql
+++ BD_BibliotecaMeiquer.sql
@@ -99,3 +99,60 @@ DELIMITER ;
 
 insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (1, "las aventuras de sr arduino","google.com",1);
 insert into videos(ID_Contenido, Duracion, Canal) values(1,20,"Arduino");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (2, "Tutorial armado de lego mindstorms","youtube.com",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(2,23,"Lego Mindstorms");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (3, "Partes del cuerpo humano","youtube.com",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(3,15,"MedicalHub");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (4, "TORNADO EN UNA BOTELLA - EXPERIMENTOS PARA NIÑOS","https://www.youtube.com/watch?v=qLOfffoODok&pp=ygUYZXhwZXJpbWVudG9zIHBhcmEgbmnDsW9z",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(4,3,"Maeta Marlene");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (5, "Estación Meiquer: transformar mi comunidad, jugando","https://www.youtube.com/watch?v=NbbFoqpJ7ZM",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(5,1,"Estacion Meiquer");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (6, "Cómo Hacer una Bobina de tesla (Muy fácil de hacer)","https://www.youtube.com/watch?v=PyMK_UGlGIw&pp=ygUYZXhwZXJpbWVudG9zIHBhcmEgbmnDsW9z",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(6,7,"Muy facil de hacer");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (7, "Aprende con Eddie a Sumar y Restar con Comida - Matemáticas divertidas para niños","https://www.youtube.com/watch?v=k_dC1sj9sOE&pp=ygUXbWF0ZW1hdGljYXMgcGFyYSBuacOxb3M%3D",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(7,6,"Aprende con EDDIE - El dinosaurio travieso");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (8, "UNIDADES, DECENAS Y CENTENAS - El valor posicional de los números - Matemáticas para niños","https://www.youtube.com/watch?v=eC6-OOfx1tQ&pp=ygUXbWF0ZW1hdGljYXMgcGFyYSBuacOxb3M%3D",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(8,5,"Smile and Learn");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (9, "Aprendiendo a multiplicar. La Multiplicación - Vídeos Educativos para niños","https://www.youtube.com/watch?v=YFtEaVw5k1A&pp=ygUXbWF0ZW1hdGljYXMgcGFyYSBuacOxb3M%3D",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(9,3,"Happy Learn Español");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (10, "LEGO 45544 MINDSTORMS Education EV3 Core Set (PART1)","https://www.youtube.com/watch?v=V-G9_qiH7Qc&pp=ygUbbGVnbyBtaW5kc3Rvcm1zIHBhcmEgbmnDsW9z",1);
+insert into videos(ID_Contenido, Duracion, Canal) values(10,4,"Brick Up");
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (11, "PAW Patrol. Mis primeras letras y numeros","https://www.planetadelibros.com.mx/libro-paw-patrol-mis-primeras-letras-y-numeros/374404",2);
+insert into libros(ID_Contenido, Paginas) values(11, 56);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (12, "PAW Patrol. Mis primeras letras y numeros","https://www.planetadelibros.com.mx/libro-paw-patrol-mis-primeras-letras-y-numeros/374404",2);
+insert into libros(ID_Contenido, Paginas) values(12, 56);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (13, "Las pistas de Blue y tú. ¡Arcoíris de pistas!","https://www.planetadelibros.com.mx/libro-las-pistas-de-blue-y-tu-arcoiris-de-pistas/371652",2);
+insert into libros(ID_Contenido, Paginas) values(13, 128);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (14, "Marcus Pocus 1. Magia a domicilio","https://www.planetadelibros.com.mx/libro-marcus-pocus-1-magia-a-domicilio/372889",2);
+insert into libros(ID_Contenido, Paginas) values(14, 128);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (15, "Princesa Piquiriqui y la (espantosa) verdad sobre los bebés","https://www.planetadelibros.com.mx/libro-princesa-piquiriqui-y-la-espantosa-verdad-sobre-los-bebes/369395",2);
+insert into libros(ID_Contenido, Paginas) values(15, 40);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (16, "Las aventuras de Dani y Evan. La isla de los pterosaurios","https://www.planetadelibros.com.mx/libro-las-aventuras-de-dani-y-evan-la-isla-de-los-pterosaurios/377417",2);
+insert into libros(ID_Contenido, Paginas) values(16, 144);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (17, "Gigantosaurus. Cuentos de 5 minutos","https://www.planetadelibros.com.mx/libro-gigantosaurus-cuentos-de-5-minutos/360977",2);
+insert into libros(ID_Contenido, Paginas) values(17, 192);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (18, "No te comas este libro","https://www.planetadelibros.com.mx/libro-no-te-comas-este-libro/372138",2);
+insert into libros(ID_Contenido, Paginas) values(18, 56);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (19, "Stuntboy. El superhéroe de los superhéroes","https://www.planetadelibros.com.mx/libro-stuntboy-el-superheroe-de-los-superheroes/363698",2);
+insert into libros(ID_Contenido, Paginas) values(19, 272);
+
+insert into contenido(ID_Contenido, Nombre, URL, Tipo) values (20, "Toy Story. Mi primer libro de arte","https://www.planetadelibros.com.mx/libro-toy-story-mi-primer-libro-de-arte/360973",2);
+insert into libros(ID_Contenido, Paginas) values(20, 72);
