d44b080254b8c50cd450b0e5c44d6c1bd4bdc6ad
Samuel117 <samuel.padilla45@gmail.com>
2023-06-14 20:03:42 -0600

    endpoint videos mejorado

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,22 @@ app.use(cors({origin:true}))
 // Cambiar username y contrasena en deploy
 const db = mysql.createConnection({
     host: "localhost",
-    user: "biblioteca",
-    password:"password",
+    user: "Samuel",
+    password:"12345",
     database: "bibliotecameiquer"
 })
 
 // Alumnos solo pueden acceder a videos y libros
 app.get("/Alumno", (req, res)=>{
     const q = "SELECT * FROM contenido WHERE tipo=1 OR tipo=2;"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.get("/Videos", (req, res)=>{
+    const q = "SELECT contenido.ID_Contenido, Nombre, URL, Duracion, Canal FROM contenido INNER JOIN videos WHERE contenido.Tipo =1 AND contenido.ID_Contenido = videos.ID_Contenido"
     db.query(q,(err,data)=>{
         if(err) return res.json(err)
         return res.json(data)
